f0a87d67ef72b607fd4cc5598144e1dd7c746d27
Saadi-Official927 <124785675+Saadi-Official927@users.noreply.github.com>
2025-02-03 16:46:06 +0500

    url change

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://backendv2.bidups.com/faiq/api',
+    baseURL: 'https://backendv2.zing.tel/faiq/api',
 });
 
 export default axiosInstance;
